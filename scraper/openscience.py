1d3676a6a203e84ef7cf368d085a543619e30754
LukaDarsalia <lukadarsalia13@gmail.com>
2025-01-21 21:58:38 +0400

    adds openscience

---
+++ scraper/openscience.py
@@ -0,0 +1,29 @@
+import time
+
+import requests
+from scraper.scraper_abc import ScraperABC
+from requests_tor import RequestsTor
+import random
+
+rt = RequestsTor(autochange_id=50)
+
+class CustomScraper(ScraperABC):
+    """
+    Simple scraper that downloads JSON content from URLs.
+    """
+    def scrape_url(self, url):
+        """
+        Download the PDF content of a URL.
+        :param url: URL to scrape.
+        :return: Tuple (file_name, file_content).
+        """
+        try:
+            time.sleep(random.uniform(0, 2))
+            response = rt.get(url, timeout=10)
+            response.raise_for_status()
+
+            file_name = f"{url.split('/')[-2]}.pdf"
+            return file_name, response.content
+        except requests.RequestException as e:
+            self.logger.error(f"Failed to fetch {url}: {e}")
+            raise
